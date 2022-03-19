9bfe34240ea1bba7037ad3ee56e71c2e7ab56909
RootSong <sgh8774@gmail.com>
2022-03-20 02:04:34 +0900

    applied correlation between entities

--- src/main/java/jpashop/JpaMain.java
+++ src/main/java/jpashop/JpaMain.java
@@ -1,12 +1,11 @@
 package jpashop;
 
-import hellojpa.Member;
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import jpashop.domain.Order;
+import jpashop.domain.OrderItem;
 
 public class JpaMain {
 
@@ -19,10 +18,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Order order = em.find(Order.class, 1L);
-            Long memberId = order.getMemberId();
-
-            Member member = em.find(Member.class, memberId);
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
